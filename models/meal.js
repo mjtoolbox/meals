ef9377595253cc11cd6c8be3e4ac01f84e5a08f6
mjtoolbox <michael.jo@gmail.com>
2020-11-11 19:16:11 -0800

    Meal details in FlatList

---
+++ models/meal.js
@@ -0,0 +1,30 @@
+class Meal {
+  constructor(
+    id,
+    categoryIds,
+    title,
+    affordability,
+    complexity,
+    imageUrl,
+    duration,
+    ingredients,
+    steps,
+    isGluteenFree,
+    isVegetarian,
+    isLactoseFree
+  ) {
+    this.id = id;
+    this.categoryIds = categoryIds;
+    this.title = title;
+    this.affordability = affordability;
+    this.complexity = complexity;
+    this.imageUrl = imageUrl;
+    this.duration = duration;
+    this.ingredients = ingredients;
+    this.steps = steps;
+    this.isGluteenFree = isGluteenFree;
+    this.isVegetarian = isVegetarian;
+    this.isLactoseFree = isLactoseFree;
+  }
+}
+export default Meal;
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,27 +1,35 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { CATEGORIES } from '../data/dummy-data';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMeals = (props) => {
+  const renderMealItem = (itemData) => {
+    return (
+      <MealItem
+        onSelectMeal={() => {}}
+        title={itemData.item.title}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        image={itemData.item.imageUrl}
+      />
+    );
+  };
+
   const catId = props.navigation.getParam('categoryId');
 
-  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+  // const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
 
   return (
     <View style={styles.screen}>
-      <Text>Cagegory Meal Screen</Text>
-      <Text>{selectedCategory.title}</Text>
-      <Button
-        title='Go to Details'
-        onPress={() => {
-          props.navigation.navigate('MealDetail');
-        }}
-      />
-      <Button
-        title='Go Back'
-        onPress={() => {
-          props.navigation.goBack();
-        }}
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
       />
     </View>
   );
