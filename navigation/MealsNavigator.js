8487950472b6fd07d9cf2059ea34d7c759ca55ee
mjtoolbox <michael.jo@gmail.com>
2020-11-11 20:53:38 -0800

    Adding icon to tab navigation & custom text

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createAppContainer } from 'react-navigation';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoriteScreen';
 import Colors from '../constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
 const MealsNavigator = createStackNavigator(
   {
@@ -28,4 +32,39 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons
+              name='ios-restaurant'
+              size={25}
+              color={tabInfo.tintColor}
+            />
+          );
+        },
+      },
+    },
+    Favorites: {
+      screen: FavoritesScreen,
+      navigationOptions: {
+        tabBarLabel: 'Favorites!',
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
+          );
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  }
+);
+
+export default createAppContainer(MealsFavTabNavigator);
